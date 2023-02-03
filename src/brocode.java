21a75173b5732bb03e188ace649206a265cb3408
RockyKing <rockymars8@gmail.com>
2023-02-03 09:05:37 -0400

    change today

---
+++ src/brocode.java
@@ -0,0 +1,18 @@
+import javax.swing.plaf.basic.BasicScrollPaneUI.ViewportChangeHandler;
+
+public class brocode {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 3;
+
+        int z = add(x, y);
+        System.out.println(z);
+    }
+
+    static int add(int x, int y) {
+        int another = x + y;
+        return another;
+
+    }
+
+}
--- src/pata.java
+++ src/pata.java
@@ -1,5 +1,11 @@
 public class pata {
     public static void main(String[] args) throws Exception {
-        System.out.println("Pata de la Suerte");
+        System.out.println("Accediendo elementos en un array");
+
+        String[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+
     }
 }
